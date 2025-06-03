26c0851065a257de8fe31c023522b692c6a52706
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-06-03 14:23:33 +0300

    Update script.js
    Well Formatted.

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ document.querySelector(".dice .img1").setAttribute("src", "images/dice" + random
 var randomNumber2 = Math.random() * 6;
 randomNumber2 = Math.floor(randomNumber2) + 1;
 
-document.querySelector(".dice .img2").setAttribute("src", "images/dice"+ randomNumber2 + ".png");
+document.querySelector(".dice .img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").textContent = "🏆 Player 1 Wins!";
 }
-else if(randomNumber1 === randomNumber2){
+else if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").textContent = "Draw! 🎲";
 }
 
-else{
+else {
     document.querySelector("h1").textContent = "Player 2 Wins! 🏆";
 }
